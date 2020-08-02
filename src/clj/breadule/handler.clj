3113841795f3af42c384812dcf1eb1ab08aa55e8
ertdfgcb <odaypaul@gmail.com>
2020-08-01 17:01:44 -0700

    finish api endpoints

--- src/clj/breadule/handler.clj
+++ src/clj/breadule/handler.clj
@@ -2,22 +2,20 @@
   (:require
    [compojure.core :refer [GET POST defroutes]]
    [compojure.route :refer [resources]]
-   [ring.util.response :refer [resource-response response]]
+   [ring.util.response :refer [resource-response]]
    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-   [ring.middleware.content-type :refer [wrap-content-type]]
    [ring.middleware.reload :refer [wrap-reload]]
+   [ring.middleware.json :refer [json-response]]
    [shadow.http.push-state :as push-state]
-  ;  [ring.util.json-response :refer [json-response]]
    [breadule.mongo :refer [insert-schedule get-all-schedules]]))
 
 (defroutes routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
-  (GET "/api/schedules" [] (response (get-all-schedules)))
+  (GET "/api/schedules" [] (json-response (get-all-schedules) {:pretty false}))
   (POST "/api/schedules" req (let [res (:params req)]
                                (insert-schedule res)))
   (resources "/"))
 
 (def dev-handler (-> #'routes wrap-reload push-state/handle))
 
-; (def handler (wrap-defaults routes (assoc site-defaults :security false)))
-(def handler routes)
+(def handler (wrap-defaults routes (assoc site-defaults :security false)))
